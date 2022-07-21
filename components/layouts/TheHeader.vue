85e2a9ef409974c73007153c909a635e0a2980d8
Vitaly Baev <vitalij.baev.98@bk.ru>
2022-07-21 12:40:10 +0300

    Старт

---
+++ components/layouts/TheHeader.vue
@@ -0,0 +1,54 @@
+<template>
+  <header class="header">
+    <div class="container">
+      <div class="header__inner">
+
+        <div class="header__logo logo">
+          <nuxt-link to="/">IMPULS</nuxt-link>
+        </div>
+
+        <nav class="nav">
+          <ul class="nav__list">
+            <li class="nav__item" v-for="item in menu" :key="item.id">
+              <nuxt-link class="nav__link" exact-active-class="m-active" :to="item.path">
+                {{ item.name }}
+              </nuxt-link>
+            </li>
+          </ul>
+        </nav>
+
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menu: [
+        {
+          id: 1,
+          name: "Главная",
+          path: "/",
+        },
+        {
+          id: 2,
+          name: "Каталог",
+          path: "/catalog",
+        },
+        {
+          id: 3,
+          name: "Как заказать",
+          path: "/order-instruction",
+        },
+        {
+          id: 4,
+          name: "О компании",
+          path: "/about",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <h1 class="title">Главная <br> в разработке</h1>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "IndexPage",
+};
+</script>
+
+<style></style>
